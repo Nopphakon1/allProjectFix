0a7132571a8e97f3df270fbcd3c36110f14eab5c
Nopphakon1 <90256923+Nopphakon1@users.noreply.github.com>
2021-09-09 22:34:47 +0700

    Add files via upload

---
+++ HTML/1_element/script.js
@@ -0,0 +1,20 @@
+body=document.getElementById("body")
+header=document.getElementById("header")
+
+nameMe="fix"
+
+function bgRed() {
+    // body.style.color='red';
+    body.style.backgroundColor='red'
+    header.innerHTML=nameMe;
+
+}
+
+
+
+function bgBlack() {
+    // body.style.color='black'
+    body.style.backgroundColor='black';
+   
+    }
+    
